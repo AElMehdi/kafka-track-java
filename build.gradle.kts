d484caf0400ddef9e08cfd5cd90c949098889e56
AElMehdi <assirelmehdi@gmail.com>
2020-03-13 16:09:32 +0100

    Add Avro and confluent gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,23 @@ plugins {
     java
     application
 
-//    id("com.commercehub.gradle.plugin.avro") version "1.3.4"
+    id("com.commercehub.gradle.plugin.avro") version "0.9.1"
 }
 
 repositories {
     jcenter()
     mavenCentral()
 
-//    maven(url = "http://packages.confluent.io")
+    // Needed for avro serde dependency
+    maven(url = "http://packages.confluent.io/maven")
 }
 
 dependencies {
-//    compile("org.apache.avro:avro:1.8.2")
-//    implementation("io.confluent:kafka-streams-avro-serde:5.2.0")
+    implementation("org.apache.kafka:kafka-streams:2.2.0")
+
+    compile("org.apache.avro:avro:1.8.2")
+    implementation("io.confluent:kafka-streams-avro-serde:5.2.1")
+
 
     // Use JUnit Jupiter API for testing.
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
@@ -22,7 +26,7 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
 
     testImplementation("org.assertj:assertj-core:3.15.0")
-//    testCompile("org.apache.kafka:kafka-streams-test-utils:2.4.1")
+    testCompile("org.apache.kafka:kafka-streams-test-utils:2.4.1")
 }
 
 application {
